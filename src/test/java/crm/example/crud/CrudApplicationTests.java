add28b3126f89ab754ffe04a9c53beae737b60ec
Donfack <Donfackfresnel4@gmail.com>
2022-03-03 18:01:50 +0100

    ongoing

---
+++ src/test/java/crm/example/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package crm.example.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
